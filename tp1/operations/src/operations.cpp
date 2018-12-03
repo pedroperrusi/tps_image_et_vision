7d800367cbc5e7ada04733a15b03b28fe9801356
SURUAGY PERRUSI Pedro Henrique <suruagyperrusi@a502pc09.psi.ad.unistra.fr>
2018-12-03 16:27:22 +0100

    Simple image callback functions

--- tp1/operations/src/operations.cpp
+++ tp1/operations/src/operations.cpp
@@ -4,9 +4,10 @@
 /*
 Global variables
 */
-cv::Mat imgIn, imgGray, imgBlur, imgCanny, detected_edges;
+cv::Mat imgIn, imgGray, imgBlur, imgCanny, mouseImage;
 std::string blurWindowName = "Blurred Image";
 std::string edgesWindowName = "Edges Image";
+std::string mouseWindowName = "Drawing Image";
 // gaussian blur
 const int maxSigmaX = 90;
 int sigmaX = 1;
@@ -23,7 +24,7 @@ int ratio = 3;
 /**
  * @function trackbar callback for sigmaX
  */
-void onTrackbarSigmaX( int value, void* )
+static void onTrackbarSigmaX( int value, void* )
 {
     sigmaX = value;
     cv::GaussianBlur(imgIn, imgBlur, cv::Size(kernel_size, kernel_size), sigmaX, sigmaY);
@@ -33,7 +34,7 @@ void onTrackbarSigmaX( int value, void* )
 /**
  * @function trackbar callback for sigmaY
  */
-void onTrackbarSigmaY( int value, void* )
+static void onTrackbarSigmaY( int value, void* )
 {
     sigmaX = value;
     cv::GaussianBlur(imgIn, imgBlur, cv::Size(kernel_size, kernel_size), sigmaX, sigmaY);
@@ -43,7 +44,7 @@ void onTrackbarSigmaY( int value, void* )
 /**
  * @function trackbar callback for kernel
  */
-void onTrackbarKernel( int value, void* )
+static void onTrackbarKernel( int value, void* )
 {
     kernel_size = 2*kernel_size+1;
     cv::GaussianBlur(imgIn, imgBlur, cv::Size(kernel_size, kernel_size), sigmaX, sigmaY);
@@ -54,20 +55,39 @@ void onTrackbarKernel( int value, void* )
  * @function CannyThreshold
  * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
  */
-void CannyThreshold(int, void*)
+static void CannyThreshold(int, void*)
 {
-  /// Reduce noise with a kernel 3x3
-  cv::blur( imgGray, detected_edges, cv::Size(3,3) );
+    cv::Mat detected_edges;
+    /// Reduce noise with a kernel 3x3
+    cv::blur( imgGray, detected_edges, cv::Size(3,3) );
 
-  /// Canny detector
-  cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, 3 );
+    /// Canny detector
+    cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, 3 );
 
-  /// Using Canny's output as a mask, we display our result
-  imgCanny = cv::Scalar::all(0);
+    /// Using Canny's output as a mask, we display our result
+    imgCanny = cv::Scalar::all(0);
 
-  imgIn.copyTo( imgCanny, detected_edges);
-  cv::imshow( edgesWindowName, imgCanny );
+    imgIn.copyTo( imgCanny, detected_edges);
+    cv::imshow( edgesWindowName, imgCanny );
  }
+
+/*
+* @function on_mouse callback for mouse interface
+*/
+static void on_mouse(int event, int x, int y, int, void*)
+{
+    // if click droit, reset mouseImage to original image
+    if(event == cv::EVENT_RBUTTONDOWN)
+    {
+        imgIn.copyTo(mouseImage);
+    }
+    if(event == cv::EVENT_LBUTTONDOWN)
+    {
+        cv::Point pt(x, y);
+        cv::circle(mouseImage, pt, 5, cv::Scalar(0,0,255));
+    }
+    cv::imshow(mouseWindowName, mouseImage);
+}
 
 /*
     Operations des Images
@@ -125,6 +145,16 @@ int main()
     CannyThreshold(0, 0);
 
     std::cout << "Showing edges image... \nPress n to continue" << std::endl;
+    while(char c = cv::waitKey(0) != 'n');
+
+    /* Exploring Mouse Callback */
+    std::cout << "Showing original image. May the drawing begin!" << std::endl;
+    imgIn.copyTo(mouseImage);
+    /// Create a window
+    cv::namedWindow( mouseWindowName, cv::WINDOW_AUTOSIZE );
+    cv::setMouseCallback( mouseWindowName, on_mouse, 0 );
+    cv::imshow(mouseWindowName, mouseImage);
+
     while(char c = cv::waitKey(0) != 'n');
 
     return 0;
